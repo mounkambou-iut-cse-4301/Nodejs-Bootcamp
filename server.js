00c26fe6d6c2afafcbe30c1d0d3d8ab836814911
Abdel Karim Mounkambou <abdelkarim@iut-dhaka.edu>
2022-11-10 11:04:31 +0100

    Create simple Tour

---
+++ server.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+const app = require('./app');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.DATABASE);
+  console.log('DB connection successful!')
+  // use `await mongoose.connect('mongodb://user:password@localhost:27017/test');` if your database has auth enabled
+}
+
+const tourSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,'A tour must have a name'],
+        unique:true
+    },
+    rating:{
+        type:Number,
+        default:4.5
+    },
+    price:{
+        type:String,
+        required:[true,'A tour must have a price'],
+    }
+});
+
+const Tour=mongoose.model('Tour',tourSchema);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
