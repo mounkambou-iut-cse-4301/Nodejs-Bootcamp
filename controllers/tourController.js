b87f0125703b2da97db00a1342fd0be0441c208c
Abdel Karim Mounkambou <abdelkarim@iut-dhaka.edu>
2022-11-13 18:52:05 +0100

    Refactoring the code

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,14 +1,23 @@
 const Tour=require('./../models/tourModel')
+const APIFeatures = require('./../utils/apiFeatures');
+exports.aliasTopTours = (req, res, next) => {
+    req.query.limit = '5';
+    req.query.sort = '-ratingsAverage,price';
+    req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+    next();
+  };
 
+
+  
 exports.getAllTours = async (req, res) => {
     try {
       // EXECUTE QUERY
-    //   const features = new APIFeatures(Tour.find(), req.query)
-    //     .filter()
-    //     .sort()
-    //     .limitFields()
-    //     .paginate();
-      const tours = await Tour.find();
+      const features = new APIFeatures(Tour.find(), req.query)
+        .filter()
+        .sort()
+        .limitFields()
+        .paginate();
+      const tours = await features.query;
   
       // SEND RESPONSE
       res.status(200).json({
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,9 +5,9 @@ const router = express.Router();
 
 // router.param('id', tourController.checkID);
 
-// router
-//   .route('/top-5-cheap')
-//   .get(tourController.aliasTopTours, tourController.getAllTours);
+router
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
 
 // router.route('/tour-stats').get(tourController.getTourStats);
 // router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
