13178d6d8c0745b1d8c234af76bc1aecb5cc5a4f
Abdel Karim Mounkambou <abdelkarim@iut-dhaka.edu>
2022-11-13 19:21:01 +0100

    Aggregation Pipeline: Matching and Grouping

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -9,7 +9,7 @@ router
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTours);
 
-// router.route('/tour-stats').get(tourController.getTourStats);
+router.route('/tour-stats').get(tourController.getTourStats);
 // router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
 router
