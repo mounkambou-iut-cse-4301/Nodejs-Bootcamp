901948380fc72d3084f34efb5e4c08e7930b1a94
Abdel Karim Mounkambou <abdelkarim@iut-dhaka.edu>
2022-11-15 10:11:32 +0100

    Handling Unhandled Routes

--- models/tourModel.js
+++ models/tourModel.js
@@ -116,7 +116,7 @@ tourSchema.pre(/^find/, function (next) {
 
 tourSchema.post(/^find/, function (docs, next) {
   console.log(`Query took ${Date.now() - this.start} milliseconds!`);
-  console.log(docs);
+//   console.log(docs);
   next();
 });
 
