988844c5cea1e7a0b7ed5d1bc6cb8d67b41a4572
Abdel Karim Mounkambou <abdelkarim@iut-dhaka.edu>
2022-11-19 13:10:04 +0100

    Signing up Users

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,22 @@
-const User=require('./../models/userModel');
-const catchAsync=require('./../utils/catchAsync');
+const User = require("./../models/userModel");
+const catchAsync = require("./../utils/catchAsync");
+const jwt = require("jsonwebtoken");
+exports.signup = catchAsync(async (req, res, next) => {
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
 
-
-exports.signup=catchAsync(async(req,res,next)=>{
-    const newUser= await User.create(req.body);
-    res.status(201).json({
-        status:'success',
-        data:{
-            user:newUser
-        }
-    })
+  const token=jwt.sign({id:newUser._id},process.env.JWT_SECRET,{
+    expiresIn:process.env.JWT_EXPIRES_IN
+  });
+  res.status(201).json({
+    status: "success",
+    token,
+    data: {
+      user: newUser,
+    },
+  });
 });
