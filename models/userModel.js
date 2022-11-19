cc3d02ab7ccf938c88d3e8019afc1e9e30303e29
Abdel Karim Mounkambou <abdelkarim@iut-dhaka.edu>
2022-11-19 08:38:31 +0100

    Managing Passwords

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const validator=require('validator')
-
+const bcript = require('bcryptjs');
 const userSchema=new mongoose.Schema(
     {
         name:{
@@ -23,9 +23,31 @@ const userSchema=new mongoose.Schema(
         passwordConfirm:{
             type:String,
             required:[true,'Please tell us your password'],
+            validate:{
+
+                //This only work on SAVE// Create
+                validator:function(el){
+                    return el===this.password; 
+                },
+                message:'Password does not match'
+
+            }
         }
     }
 )
+
+//Add document middleware: runs before .save() and .create()
+userSchema.pre("save", async function (next) {
+
+    // Only run this function if password was modified
+    if(!this.isModified('password')) return next()
+//encription
+this.password= await bcript.hash(this.password,12)
+
+//delete the passwordConfirm field
+this.passwordConfirm=undefined;
+next();
+  });
 
 const User = mongoose.model("User", userSchema);
 
