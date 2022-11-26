ef7ac20b95c51cce1c8688550df27cd66e465680
Abdel Karim Mounkambou <abdelkarim@iut-dhaka.edu>
2022-11-26 08:25:24 +0100

    Password Reset Functionality: Setting New Password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,14 @@
-const {promisify}=require('util')
+const crypto = require("crypto");
+const { promisify } = require("util");
 const User = require("./../models/userModel");
 const catchAsync = require("./../utils/catchAsync");
 const jwt = require("jsonwebtoken");
 const AppError = require("./../utils/AppError");
-const { log } = require('console');
+const { log } = require("console");
 const sendEmail = require("./../utils/email");
 
 const signToken = (id) => {
-  return jwt.sign({ id:id }, process.env.JWT_SECRET, {
+  return jwt.sign({ id: id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 };
@@ -17,7 +18,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     email: req.body.email,
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
-    role: req.body.role
+    role: req.body.role,
   });
 
   const token = signToken(newUser._id);
@@ -49,84 +50,115 @@ exports.login = catchAsync(async (req, res, next) => {
 
 exports.protect = catchAsync(async (req, res, next) => {
   // Get the token if exist
-  let token
+  let token;
   if (
-    req.headers.authorization 
+    req.headers.authorization
     // &&
     // req.headers.authorization.startWith('Bearer')
   ) {
-     token = req.headers.authorization.split(" ")[1];
+    token = req.headers.authorization.split(" ")[1];
   }
-  if(!token){
-    return next(new AppError("You are not logged in! Please log in to get access", 401))
+  if (!token) {
+    return next(
+      new AppError("You are not logged in! Please log in to get access", 401)
+    );
   }
   // Verification token
 
   //This return a promise
-const decoded= await promisify(jwt.verify)(token,process.env.JWT_SECRET)
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
 
   //Check if user still exists
   const currentUser = await User.findById(decoded.id);
-  if(!currentUser){
-    return next(new AppError("The user belonging to this token does no longer exist.", 401));
+  if (!currentUser) {
+    return next(
+      new AppError(
+        "The user belonging to this token does no longer exist.",
+        401
+      )
+    );
   }
-
 
   //Check if user change password after the token was issued
   if (currentUser.changedPasswordAfter(decoded.iat)) {
     return next(
-      new AppError('User recently changed password! Please log in again.', 401)
+      new AppError("User recently changed password! Please log in again.", 401)
     );
   }
 
   //Grand access to protected route
-  req.user=currentUser;
+  req.user = currentUser;
   next();
 });
 
-exports.restrictTo=(...roles)=>{
-  return (req,res,next)=>{
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
     // roles [admin,lead-guide]
-    if(!roles.includes(req.user.role)){
-      return next(new AppError('You do not have permission to access this action.', 403))
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError("You do not have permission to access this action.", 403)
+      );
     }
-next();
-  }
-}
+    next();
+  };
+};
 
 exports.forgotPassword = catchAsync(async (req, res, next) => {
-//1 Get user based on post email
-const user =await User.findOne({email:req.body.email})
-if(!user){
-  return next(new AppError('There is no user with this email addess.',404))
-}
+  //1 Get user based on post email
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return next(new AppError("There is no user with this email addess.", 404));
+  }
 
-//2 Generate the random reset token
-const resetToken=user.createPasswordReset();
-// save reset Token
-await user.save({validateBeforeSave:false}); // Will desactivate all the validators we have on our schema
+  //2 Generate the random reset token
+  const resetToken = user.createPasswordReset();
+  // save reset Token
+  await user.save({ validateBeforeSave: false }); // Will desactivate all the validators we have on our schema
 
-
-// 3 send email
-const resetURL = `${req.protocol}://${req.get(
-    'host'
+  // 3 send email
+  const resetURL = `${req.protocol}://${req.get(
+    "host"
   )}/api/v1/users/resetPassword/${resetToken}`;
 
   const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
 
- 
-    await sendEmail({
-      email: user.email,
-      subject: 'Your password reset token (valid for 10 min)',
-      message
-    });
+  await sendEmail({
+    email: user.email,
+    subject: "Your password reset token (valid for 10 min)",
+    message,
+  });
 
-    res.status(200).json({
-      status: 'success',
-      message: 'Token sent to email!'
-    });
- 
+  res.status(200).json({
+    status: "success",
+    message: "Token sent to email!",
+  });
+});
+exports.resetPassword = catchAsync(async (req, res, next) => {
+  // 1 get user by token
+  const hashedToken = crypto
+    .createHash("sha256")
+    .update(req.params.token)
+    .digest("hex");
 
-})
-exports.resetPassword = catchAsync(async (req, res, next) => {})
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() },
+  });
+  //2 if token has not expired and there is user, set the new pass
+  if(!user){
+    return next(new AppError("Token is invalid or has expired.", 404));
+  }
+  user.password=req.body.password;
+  user.passwordConfirm=req.body.passwordConfirm;
+  user.passwordResetToken=undefined;
+  user.passwordResetExpires=undefined;
+  await user.save();
+  //3 update changePassword property for the user
 
+  //4 log the user in and sent JWT
+  const token = signToken(user._id);
+  res.status(200).json({
+    status: "success",
+    token,
+  });
+});
--- models/userModel.js
+++ models/userModel.js
@@ -55,6 +55,12 @@ userSchema.pre("save", async function (next) {
   this.passwordConfirm = undefined;
   next();
 });
+userSchema.pre('save',function(next){
+if(!this.isModified("password") || this.isNew) return next();
+
+this.passwordChangedAt=Date.now()-1000;
+next();
+});
 
 // Check password. Instance method and will be available on all docs
 userSchema.methods.correctPassword = async function (
