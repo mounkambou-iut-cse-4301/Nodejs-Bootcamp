8cdc5d2561238712ed6096b49ac20a9d2bed8835
Abdel Karim Mounkambou <abdelkarim@iut-dhaka.edu>
2022-11-28 06:52:26 +0100

    Data Sanitization

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit')
 const helmet = require("helmet");
-
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+// const hpp = require('hpp');
 const AppError=require('./utils/AppError')
 const globalErrorHandler=require('./controllers/errorController')
 const tourRouter = require('./routes/tourRoutes');
@@ -33,6 +35,12 @@ app.use(limiter)
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
 app.use(express.static(`${__dirname}/public`));
 
 
