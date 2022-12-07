8fa3f79d93ee707d16e09ffd714e2b3ee3242fcf
Abdel Karim Mounkambou <abdelkarim@iut-dhaka.edu>
2022-12-07 11:11:47 +0100

    Populating Tour Guides

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -34,8 +34,9 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 
   exports.getTour = catchAsync(async (req, res, next) => {
 
-      const tour = await Tour.findById(req.params.id);
+      const tour = await Tour.findById(req.params.id).populate('guides');
       // Tour.findOne({ _id: req.params.id })
+      console.log(tour);
   if(!tour){
     return next(new AppError('No tour found with that ID',400))
   }
