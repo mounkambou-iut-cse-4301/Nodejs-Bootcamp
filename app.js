ebcf7f94a5dbf06bfb5c6e70abb8c60213a8563a
Abdel Karim Mounkambou <abdelkarim@iut-dhaka.edu>
2022-12-08 10:12:02 +0100

    Creating and Getting Reviews

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const AppError=require('./utils/AppError')
 const globalErrorHandler=require('./controllers/errorController')
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 // Global middleware
@@ -66,6 +67,7 @@ app.use((req, res, next) => {
 // 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 //Handling Unhandled Routes
 app.all('*',(req,res,next)=>{
---
+++ controllers/reviewController.js
@@ -0,0 +1,25 @@
+const Review = require('./../models/reviewModel');
+// const factory = require('./handlerFactory');
+const catchAsync = require('./../utils/catchAsync');
+
+exports.getAllReviews=catchAsync(async(req,res,next)=>{
+    const reviews=await Review.find();
+
+    res.status(200).json({
+        status:'sucess',
+        results:reviews.length,
+        data:{
+            reviews
+        }
+    })
+})
+
+exports.createReview=catchAsync(async(req,res,next)=>{
+    const newReview=await Review.create(req.body);
+    res.status(200).json({
+        status:'sucess',
+        data:{
+            review:newReview
+        }
+    })
+})
