a3c497d6ba8cda7d81e443139433e52dd5b0e954
Abdel Karim Mounkambou <abdelkarim@iut-dhaka.edu>
2022-12-10 14:53:30 +0100

    Adding a /me Endpoint

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,6 +9,7 @@ router.post('/login',authController.login)
 router.post('/forgotPassword',authController.forgotPassword)
 router.patch('/resetPassword/:token',authController.resetPassword)
 router.patch('/updateMyPassword',authController.protect,authController.updatePassword)
+router.get('/me', authController.protect, userController.getMe,userController.getUser);
 
 router.patch('/updateMe', authController.protect, userController.updateMe);
 router.delete('/deleteMe', authController.protect, userController.deleteMe);
