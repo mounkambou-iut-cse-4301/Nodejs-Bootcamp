deb1431422fb1865acf3966efa25705a0e571908
Abdel Karim Mounkambou <abdelkarim@iut-dhaka.edu>
2022-12-11 19:27:33 +0100

    Adding Missing Authentication and Authorization

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -10,7 +10,6 @@ router
   .route('/')
   .get(reviewController.getAllReviews)
   .post(
-    authController.protect,
     authController.restrictTo('user'),
     reviewController.setTourUserIds,
     reviewController.createReview
