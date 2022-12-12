5791f7dc38c691882de8f98d150c4bc96936b4dd
Abdel Karim Mounkambou <abdelkarim@iut-dhaka.edu>
2022-12-12 15:12:55 +0100

    Importing Review and User Data

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -2,7 +2,8 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const Tour = require('./../../models/tourModel');
-
+const Review = require('./../../models/reviewModel');
+const User = require('./../../models/userModel');
 dotenv.config({ path: './config.env' });
 
 main().catch(err => console.log(err));
@@ -13,32 +14,39 @@ async function main() {
   // use `await mongoose.connect('mongodb://user:password@localhost:27017/test');` if your database has auth enabled
 }
 const tours=JSON.parse(fs.readFileSync(`${__dirname}/tours.json`,'utf-8'));
+const users=JSON.parse(fs.readFileSync(`${__dirname}/users.json`,'utf-8'));
+const reviews=JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`,'utf-8'));
 
-// Import data into db
-const importData=async()=>{
-    try{
-        await Tour.create(tours);
-        console.log('Data successfully loaded');
-        process.exit();
-    }catch(err){
-        console.log(err);
+// IMPORT DATA INTO DB
+const importData = async () => {
+    try {
+      await Tour.create(tours);
+      await User.create(users, { validateBeforeSave: false });
+      await Review.create(reviews);
+      console.log('Data successfully loaded!');
+    } catch (err) {
+      console.log(err);
     }
-}
-
-// DElete all data from DB
-const deleteData=async()=>{
-    try{
-        await Tour.deleteMany();
-        console.log('Data successfully deleted');
-        process.exit();
-    }catch(err){
-        console.log(err);
+    process.exit();
+  };
+  
+  // DELETE ALL DATA FROM DB
+  const deleteData = async () => {
+    try {
+      await Tour.deleteMany();
+      await User.deleteMany();
+      await Review.deleteMany();
+      console.log('Data successfully deleted!');
+    } catch (err) {
+      console.log(err);
     }
-}
-
-if (process.argv[2] === '--import') {
+    process.exit();
+  };
+  
+  if (process.argv[2] === '--import') {
     importData();
   } else if (process.argv[2] === '--delete') {
     deleteData();
   }
+  
 //node dev-data/data/import-dev-data.js --delete
