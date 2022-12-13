fe9353694cbbecbd246dbf7e3b3a4b62f413cf03
Abdel Karim Mounkambou <abdelkarim@iut-dhaka.edu>
2022-12-13 14:51:43 +0100

    Geospatial Aggregation: Calculating Distances

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -20,7 +20,10 @@ router.route("/monthly-plan/:year").get(  authController.protect,
   router
   .route('/tours-within/:distance/center/:latlng/unit/:unit')
   .get(tourController.getToursWithin);
-  
+  // /tours-within?distance=233&center=-40,45&unit=mi
+// /tours-within/233/center/-40,45/unit/mi
+
+router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances);
 router
   .route("/")
   .get(tourController.getAllTours)
