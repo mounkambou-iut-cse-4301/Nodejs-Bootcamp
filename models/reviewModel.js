892cbbd57462fd852b86a6bb5df6ed7d1d51ad90
Abdel Karim Mounkambou <abdelkarim@iut-dhaka.edu>
2022-12-13 14:22:59 +0100

    Preventing Duplicate Reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -33,7 +33,7 @@ const reviewSchema = new mongoose.Schema(
     toObject: { virtuals: true }
   }
 );
-
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
 reviewSchema.pre(/^find/, function(next) {
     // this.populate({
     //   path: 'tour',
--- models/tourModel.js
+++ models/tourModel.js
@@ -35,6 +35,7 @@ const tourSchema = new mongoose.Schema(
       default: 4.5,
       min: [1, "Rating must be above 1.0"],
       max: [5, "Rating must be below 5.0"],
+      set: val => Math.round(val * 10) / 10 // 4.666666, 46.6666, 47, 4.7
     },
     ratingsQuantity: {
       type: Number,
