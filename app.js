584da1f5807b38f2b448d6ab6b484d771eab3ff5
Abdel Karim Mounkambou <abdelkarim@iut-dhaka.edu>
2022-12-22 15:00:00 +0100

    Including Files into Pug Templates

--- app.js
+++ app.js
@@ -12,16 +12,15 @@ const globalErrorHandler=require('./controllers/errorController')
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
-
+const viewRouter = require('./routes/viewRoutes');
 const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
-// Global middleware
 
+// 1) GLOBAL MIDDLEWARES
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
-// 1) MIDDLEWARES
 //Setting Security HTTP Headers
 app.use(helmet());
 
@@ -72,9 +71,9 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
-app.get('/',(req,res)=>{
-  res.status(200).render('base');
-})
+app.use('/', (req,res)=>{
+  res.status(200).render('base')
+});
 
 
 app.use('/api/v1/tours', tourRouter);
